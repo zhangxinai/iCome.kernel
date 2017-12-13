e6e85b2fc7809f49accbb5b2738bf2928f7a5b94
张鑫 <973401459@qq.com>
2017-12-13 10:56:06 +0800

    测试提交 Git

--- iCome.kernel/ViewController.m
+++ iCome.kernel/ViewController.m
@@ -17,8 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    _test = @"测试提交git";
-    _test  = @"dierciceshi";
 }
 
 
